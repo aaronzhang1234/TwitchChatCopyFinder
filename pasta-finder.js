750304c25ef8d0cb04f9a663a423dbce9623ceba
aaronzhang1234 <nimbyest@gmail.com>
2018-10-23 20:57:15 -0400

    console logging message

--- pasta-finder.js
+++ pasta-finder.js
@@ -1,8 +1,10 @@
 let chat = document.getElementsByClassName('tw-flex-grow-1 tw-full-height tw-pd-b-1')[0];
 let config = { childList: true };
-let callback = function(list, observer) {
+let callback = function(list) {
     for (i of list) {
-        console.log(i);
+        let message = i.addedNodes[0].lastChild.innerText;
+        console.log(message.length);
+        console.log(decodeURI(message));
     }
 };
 let observer = new MutationObserver(callback);
