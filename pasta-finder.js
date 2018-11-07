963482e268a09856c60e1f584bd701f6eadb4848
aaronzhang1234 <nimbyest@gmail.com>
2018-11-07 17:41:26 -0500

    parses better twitch tv and emote messages

--- pasta-finder.js
+++ pasta-finder.js
@@ -1,14 +1,62 @@
+console.log('kill me');
+
+window.addEventListener('locationchange', function(){
+    console.log('location changed!');
+});
+window.onpopstate = function(event){
+    console.log(event);
+};
+window.onhashchange = function(){
+    console.log("changed hash");
+};
+
+setInterval(function(){console.log("still functioning");}, 3000);
+
+let bttvInstalled = document.getElementById("bttv-font-size");
+console.log(bttvInstalled);
 let chat = document.getElementsByClassName('tw-flex-grow-1 tw-full-height tw-pd-b-1')[0];
 let config = { childList: true };
+
 let callback = function(list) {
+    const nms = String.fromCharCode(8203); //&NegativeMediumSpace;
+    const zwnj = String.fromCharCode(8204); //&zwnj;
+    const zwj = String.fromCharCode(8205); //&zwj;
     for (i of list) {
-        let message = i.addedNodes[0].lastChild.innerText;
-        console.log(message.length);
-        console.log(decodeURI(message));
+        if(i.addedNodes[0]){
+            let new_div = i.addedNodes[0];
+            message = parseMessage(new_div);
+            let findex = message.indexOf(nms);
+            if(findex >= 0){
+                let sig = message.substring(findex,findex+18); 
+                if(sig == signature){
+                    let image = document.createElement("img"); 
+                    image.src = "https://i.imgur.com/3xIwpKb.jpg";
+                    new_div.appendChild(image);
+                    msg_span.style.background = "blue"; 
+                }
+            }
+        }
     }
 };
+function parseMessage(msg_div){
+    let message = ""
+    let chat_divs = msg_div.children; 
+    for(let i = 3; i< chat_divs.length; i++){
+        let current_chat_item = chat_divs.item(i);
+        let data_a = current_chat_item.getAttribute("data-a-target");
+        if(data_a === 'chat-message-text'){
+            let has_children = current_chat_item.firstElementChild;
+            if(has_children){
+                let bttv_node = current_chat_item.childNodes.item(0);
+                let text = bttv_node.innerText;
+                message += text + ' ';
+            }else{
+                msg_text = current_chat_item.innerHTML;
+                message += msg_text + " ";
+            }
+        }
+    }    
+    return message;
+}
 let observer = new MutationObserver(callback);
 observer.observe(chat, config);
-
-console.log("hello");
-console.log(chat);
--- popup.js
+++ popup.js
@@ -2,15 +2,43 @@ const nms = String.fromCharCode(8203); //&NegativeMediumSpace;
 const zwnj = String.fromCharCode(8204); //&zwnj;
 const zwj = String.fromCharCode(8205); //&zwj;
 
-let get_pasta = document.getElementById('get_pasta');
+let start_pasta = document.getElementById('start_pasta');
+let get_pasta   = document.getElementById('get_pasta');
+let new_sig     = document.getElementById('new_sig');
 let bkg = chrome.extension.getBackgroundPage();
 let pastaland = document.getElementById('pastaland');
+let pastawithsig = document.getElementById('pastawithsig');
 
-get_pasta.onclick = function() {
+start_pasta.onclick= function(){
     chrome.tabs.executeScript({
-        file:'pasta-finder.js'
+        code: 'var signature = null'
+    },function(){
+        chrome.tabs.executeScript({file:'pasta-finder.js'});
     });
+}
+new_sig.onclick = function() {
+    let signature = generateSignature();
+    
+    chrome.storage.local.set({signature:signature}, function(){
+        console.log(signature);
+    });
+
+    chrome.storage.local.get(['signature'], function(result){
+        var signature = {sig:result.signature};
+        chrome.tabs.executeScript({
+            code: 'var signature = ' + JSON.stringify(signature.sig)
+        });
+    })
 };
+get_pasta.onclick = function(){
+    chrome.storage.local.get(['signature'], function(result){
+        let pasta = pastaland.value;
+        let random_pos_not_floored = (pasta.length -2) * Math.random();
+        let random_pos = Math.floor(random_pos_not_floored) + 1;
+        let randomed_pasta = pasta.slice(0, random_pos) + result.signature + pasta.slice(random_pos); 
+        pastawithsig.innerHTML = randomed_pasta;
+    })
+}
 
 //return a random 16 character signature
 function generateSignature() {
@@ -23,5 +51,6 @@ function generateSignature() {
             case 2: sig += zwj; break;
         }
     }
-    return zwj + sig + zwj;
+    return nms + sig + nms;
 }
+
